07dceb34927c251f5362dd6b52b996ee0fe4e907
snow41438 <snow41438@gmail.com>
2023-08-10 17:40:29 +0530

    added icons

--- src/Navigators.tsx
+++ src/Navigators.tsx
@@ -7,20 +7,59 @@ import DashboardScreen from './screens/DashboardScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import { COLORS } from '../utils/theme';
 // import { getData } from '../utils/services';
 // import { user } from '../utils/constants';
+import { Icon } from '@rneui/themed';
+import { StyleSheet } from 'react-native';
 
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const tintColor = (focused: boolean) => {
+    return focused ? COLORS.black : COLORS.earth
+}
 
 const BottomTabs = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name='Profile' component={ProfileScreen} />
-            <Tab.Screen name='Dashboard' component={DashboardScreen} />
-            <Tab.Screen name='Settings' component={SettingsScreen} />
+            <Tab.Screen name='Dashboard' component={DashboardScreen} options={{
+                headerStyle: {
+                    backgroundColor: COLORS.grey,
+                },
+                headerTitleStyle: {
+                    color: COLORS.white
+                },
+                tabBarIcon: ({ focused }) => (
+                    <Icon
+                        name='sc-telegram'
+                        type='evilicon'
+                        color={tintColor(focused)}
+                        size={30}
+                    />
+                )
+            }} />
+            <Tab.Screen name='Profile' component={ProfileScreen} options={{
+                tabBarIcon: ({ focused }) => (
+                    <Icon
+                        name='sc-telegram'
+                        type='evilicon'
+                        color={tintColor(focused)}
+                        size={30}
+                    />
+                )
+            }} />
+            <Tab.Screen name='Settings' component={SettingsScreen} options={{
+                tabBarIcon: ({ focused }) => (
+                    <Icon
+                        name='sc-gear'
+                        type='evilicon'
+                        color={tintColor(focused)}
+                        size={30}
+                    />
+                )
+            }} />
         </Tab.Navigator>
     )
 }
@@ -42,3 +81,9 @@ export default function RootNavigation() {
         </NavigationContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    icon: {
+        fontSize: 40
+    }
+})
---
+++ utils/theme.ts
@@ -0,0 +1,10 @@
+
+export const COLORS = {
+    yellow: "#f5f0bb",
+    beige: "#dbdfaa",
+    sage: "#b3c890",
+    earth: "#73a9ad",
+    white: "#fff",
+    black: "#000",
+    grey: "#454545"
+}
